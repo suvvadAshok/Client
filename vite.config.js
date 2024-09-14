1c312d25cf84321d07da77de595f880fc2a3ea81
ashokadvait <ashok@advaitlabs.com>
2024-09-14 18:02:18 +0530

    update html

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: "public",
-  base: '/Client/',
+  publicDir: 'public',
+  base: '/Client/',  // This is correct if your GitHub Pages repo is 'Client'
 });
